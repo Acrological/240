80e9268746192b90bfb9684cdb7630405d4af633
Acrological <33267940+Acrological@users.noreply.github.com>
2023-09-10 16:51:34 -0400

    Actualizar CreateWorkout.jsx

---
+++ Frontend/my-fitness-app/src/components/ManageWorkout/CreateWorkout/CreateWorkout.jsx
@@ -0,0 +1,29 @@
+
+export const CreateWorkout = () => {
+
+const workoutRoutine = [];
+const addExercise = () => {
+//reference codees exercise list and adds it to the workout
+}
+const deleteExercise = () => {
+
+}
+
+
+//implement exercise adding feature
+return (
+ <>
+<Button onClick={}> 
++
+</>
+<Button onClick={}> 
+-
+</>
+
+
+</>
+);
+
+
+
+}
