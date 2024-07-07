4c5ab26b98c8ed703cfcb93cc03b1472ca29b8ae
Acrological <Acrotechwavy@gmail.com>
2024-07-06 22:35:31 -0400

    add routes

---
+++ Backend/routes/user.route.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+const {createUser,getAllUsers,getUser,updateUser} = require('../controllers/user.controller');
+
+router.get('/', async (req,res) => {
+    const users = await getAllUsers();
+    res.json(users);
+});
+
+router.get('/:id', async (req,res) => {
+    const user = await getUser(req.params.id);
+    res.json(user);
+});
+
+router.post('/', async (req,res) => {
+    const newUser = await createUser(req.body);
+    res.json(newUser);
+});
+
+router.put('/',async(req,res) => {
+    const updatedUser = await updateUser(req.body);
+    res.json(updatedUser)
+})
