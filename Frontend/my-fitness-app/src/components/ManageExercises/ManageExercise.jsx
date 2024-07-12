6ec90d73a785dfa80e5bdd1a660b9a9ae9cf839b
Acrological <Acrotechwavy@gmail.com>
2024-07-12 19:50:37 -0400

    refactor exercise

--- Frontend/my-fitness-app/src/components/ManageExercises/ManageExercise.jsx
+++ Frontend/my-fitness-app/src/components/ManageExercises/ManageExercise.jsx
@@ -9,14 +9,7 @@ export const ManageExercise = () => {
     const [exercises,setExercises] = useState([]);
     const [showCreateExercise,setShowCreateExercise] = useState(false);
     const [updateList,setUpdateList] = useState(false);
-    const [showUpdateBox,setShowUpdateBox] = useState(false);
-    const [currentIndex,setCurrentIndex] = useState();
-    const exerciseWeight = useRef();
-    const [showHistory,setShowHistory] = useState(false);
 
-    const openRecords = () => {
-        setShowHistory(x => !x)
-    }
 
     useEffect(()=>{
         if(exerciseList !== exercises){
@@ -33,46 +26,13 @@ export const ManageExercise = () => {
         setShowCreateExercise(false);
     }
 
-    const deleteExercise = (index) => {
-        const newList = [...exercises]
-        console.log(newList[index])
-        newList.splice(index,1);
-        console.log(newList);
-        localStorage.setItem('exercises',JSON.stringify(newList));
-        setUpdateList(x => !x);
-    }
+
 
     const updateListProp = () => {
         setUpdateList(x => !x);
     }
     
-    const updateWorkout = (index,listLength) =>{
-        const newList = [...exercises];
-        console.log(newList[index].history[listLength].weight);
 
-        setShowUpdateBox(x => !x);
-        setCurrentIndex(index);
-        const date = new Date();
-        if(showUpdateBox == true){
-        if(exercises[index].history[listLength].weight === exerciseWeight.current.value){
-            console.log('same value');
-        }
-        else if(exerciseWeight.current.value < 0){
-            console.log('no negative value');
-        } 
-        else if (exerciseWeight.current.value > 0 ){
-            const newWeight = exerciseWeight.current.value
-            newList[index].history.push({weightDate : date,
-                weight : newWeight,})
-            localStorage.setItem('exercises',JSON.stringify(newList));
-            setUpdateList(x => !x);
-            
-        } else {
-                console.log(exerciseWeight.current.value + ' equates to no value');
-        }
-        }
-
-    }
 
     return (    
     <>
