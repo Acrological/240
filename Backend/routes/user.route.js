d5cc912716c42305cdc3126c8f3b9f77801086bd
Acrological <Acrotechwavy@gmail.com>
2024-08-01 23:27:23 -0400

    file relocating

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -25,4 +25,43 @@ router.post('/login',async(req,res) => {
     const user = await loginUser(req.body);
     res.json(user);
 })
+
+// Verification of JWT
+router.get("/user/validateToken", (req, res) => {
+    // Tokens are generally passed in header of request
+    // Due to security reasons.
+
+    let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+    try {
+        const token = req.header(tokenHeaderKey);
+
+        const verified = jwt.verify(token, jwtSecretKey);
+        if (verified) {
+            return res.send("Successfully Verified");
+        } else {
+            // Access Denied
+            return res.status(401).send(error);
+        }
+    } catch (error) {
+        // Access Denied
+        return res.status(401).send(error);
+    }
+});
+
+router.post("/user/generateToken", (req, res) => {
+    // Validate User Here
+    // Then generate JWT Token
+
+    let jwtSecretKey = process.env.JWT_SECRET_KEY;
+    let data = {
+        time: Date(),
+        userId: 12,
+    }
+
+    const token = jwt.sign(data, jwtSecretKey);
+
+    res.send(token);
+});
 module.exports = router;
